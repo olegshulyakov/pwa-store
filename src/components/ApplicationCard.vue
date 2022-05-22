33158719090756cd29e6eeedd330bf531d76f0af
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2022-05-22 19:36:45 +0300

    ApplicationCard - use button instead of link

--- src/components/ApplicationCard.vue
+++ src/components/ApplicationCard.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="flex flex-col items-center py-4 rounded-xl shadow-md">
-    <img :src="app.icon" :alt="app.title" class="w-48 h-48" />
+    <img :src="app.icon" :alt="app.title" class="w-48 h-48 cursor-pointer" v-on:click="openApp" />
 
     <h4 class="my-1">{{ app.title }}</h4>
 
-    <a
-      :href="app.url"
-      target="_blank"
-      :alt="$t('applicationCard.get')"
-      class="bg-transparent font-semibold inline-flex items-center py-1 px-3 rounded-lg text-green-500 border border-green-500 hover:bg-green-500 hover:text-white border-transparent"
+    <button
+      class="bg-transparent font-semibold inline-flex items-center uppercase py-1 px-3 border rounded-lg bg-green-500 text-white border-transparent hover:bg-transparent hover:text-green-500 hover:border-green-500"
+      v-on:click="openApp"
     >
       {{ $t("applicationCard.get") }}
-    </a>
+    </button>
   </div>
 </template>
 
@@ -22,6 +20,12 @@ import type { PropType } from "vue";
 export default {
   props: {
     app: { type: Object as PropType<AppInfo>, required: true },
+  },
+  methods: {
+    openApp(event: Event) {
+      event?.preventDefault();
+      window.open(this.app.url, "_blank").focus();
+    },
   },
 };
 </script>
