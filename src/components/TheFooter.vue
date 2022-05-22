d6948e57ffbe4b87bdba4b1e1abad7c98da622aa
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2022-05-22 19:23:39 +0300

    Update Footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,17 +5,20 @@
     <nav class="mx-auto">
       <ul class="flex flex-wrap justify-center md:justify-end">
         <li>
-          <RouterLink to="/">{{ $t("footer.home") }}</RouterLink>
+          <a href="https://github.com/olegshulyakov/doesitpwa" target="_blank" class="hover:underline py-1 mr-4">
+            {{ $t("footer.contact-me") }}
+          </a>
         </li>
         <li>
-          <a href="https://github.com/olegshulyakov/doesitpwa">{{ $t("footer.contact-me") }}</a>
-        </li>
-        <li>
-          <a href="https://www.apache.org/licenses/LICENSE-2.0.txt">{{ $t("footer.terms") }}</a>
+          <a href="https://www.apache.org/licenses/LICENSE-2.0.txt" target="_blank" class="hover:underline py-1 mr-4">
+            {{ $t("footer.terms") }}
+          </a>
         </li>
         <li>
           <a
             href="https://github.com/olegshulyakov/doesitpwa/issues/new?assignees=&labels=add-application&template=submit_new_app.md&title=%5BNEW+APP%5D"
+            target="_blank"
+            class="hover:underline py-1 mr-4"
           >
             {{ $t("footer.new-app") }}
           </a>
@@ -25,9 +28,3 @@
     </nav>
   </footer>
 </template>
-
-<style lang="postcss">
-nav a {
-  @apply hover:underline py-1 mr-4;
-}
-</style>
