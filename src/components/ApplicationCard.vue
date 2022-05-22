2c0183b3575ee6fabfc05fc92c9c52e99b776618
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2022-05-22 19:23:18 +0300

    i18n ApplicationCard

--- src/components/ApplicationCard.vue
+++ src/components/ApplicationCard.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex flex-col items-center py-4 rounded-xl shadow-md">
-    <img :src="app.icon" class="w-48 h-48" />
+    <img :src="app.icon" :alt="app.title" class="w-48 h-48" />
 
     <h4 class="my-1">{{ app.title }}</h4>
 
     <a
       :href="app.url"
       target="_blank"
+      :alt="$t('applicationCard.get')"
       class="bg-transparent font-semibold inline-flex items-center py-1 px-3 rounded-lg text-green-500 border border-green-500 hover:bg-green-500 hover:text-white border-transparent"
     >
-      <span>Get</span>
+      {{ $t("applicationCard.get") }}
     </a>
   </div>
 </template>
