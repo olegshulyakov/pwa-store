e857d27bdaef338f22a2290ea98dc58276991a74
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2022-05-23 21:48:36 +0300

    #6 Create website checker on Puppetier

--- src/utils/checker.js
+++ src/utils/checker.js
@@ -1,80 +1,35 @@
 const process = require("process");
-const https = require("https");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
-const selector = {
-  title: "title",
-  description: "meta[name='description']",
-  url: "link[rel='canonical']",
-  icon: "link[rel='apple-touch-icon']",
-  manifest: "link[rel='manifest']",
-};
+async function doCheck(link) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(link);
 
-function doRequest(url) {
-  return new Promise((resolve, reject) => {
-    https.get(
+  const siteData = await page.evaluate(() => {
+    const url = document.querySelector("head link[rel='canonical']")?.href ?? window.location.href;
+    const manifest = document.querySelector("head link[rel='manifest']")?.href;
+
+    return {
       url,
-      {
-        port: 443,
-        method: "GET",
-      },
-      function (response) {
-        response.setEncoding("utf8");
-        response.on("data", function (body) {
-          resolve(body);
-        });
-        response.on("error", function (err) {
-          console.error(`Could fetch: ${url}\n${err}`);
-          reject(err);
-        });
-      }
-    );
+      active: !!manifest,
+      title: document.querySelector("head title")?.text,
+      description: document.querySelector("head meta[name='description']")?.content,
+      icon: document.querySelector("head link[rel='apple-touch-icon']")?.href,
+      images: [],
+      categories: [],
+      tags: [],
+    };
   });
-}
 
-function doCheck(url) {
-  doRequest(url)
-    .then((html) => {
-      const $ = cheerio.load(html);
-      const root = $("html");
-      const head = root.find("head").first();
+  console.log(`${JSON.stringify(siteData)},\n`);
 
-      const siteData = {
-        url: url,
-        active: false,
-        title: undefined,
-        description: undefined,
-        icon: undefined,
-        images: [],
-        categories: [],
-        tags: [],
-      };
-
-      if (head.find(selector.title).length > 0) {
-        siteData.title = head.find(selector.title).first().text();
-      }
-
-      if (head.find(selector.description).length > 0) {
-        siteData.description = head.find(selector.description).first().attr("content");
-      }
-
-      if (head.find(selector.url).length > 0) {
-        siteData.url = `${head.find(selector.url).find().attr("href")}`;
-      }
-
-      if (head.find(selector.icon).length > 0) {
-        siteData.icon = `${siteData.url.replace(/\/$/, "")}${head.find(selector.icon).first().attr("href")}`;
-      }
-
-      if (head.find(selector.manifest).length > 0) {
-        siteData.active = true;
-      }
-
-      console.log(siteData);
-    })
-    .catch((err) => console.error(`Could parse: ${url}\n${err}`));
+  await browser.close();
 }
 
 const args = process.argv.slice(2);
+
+console.log("args:", args);
+console.log("\nApps:");
 
 for (const url of args) doCheck(url);
