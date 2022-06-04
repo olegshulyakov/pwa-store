7dc26089966e54f29f4c528ffdfe93e804bb6c3b
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2022-06-04 15:21:35 +0300

    Move images

--- vite.config.ts
+++ vite.config.ts
@@ -28,17 +28,17 @@ export default defineConfig({
         lang: "en",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/images/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/images/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/images/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
